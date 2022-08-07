0d07de6d8dee55080271d15dccbc1763df2414ba
Yana Karpliuk <yanakarpliuk99@gmail.com>
2022-08-07 22:50:43 +0300

    minor fixes

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,7 +13,7 @@ export default function FactsPage(){
         <h2>Hi there!</h2>
         <p>My name is Yana and I'm a future frontend developer🔥</p>
         <p>I've already learned HTML, CSS, JavaScript. And this website is my first project created with React.</p>
-        <p>You can find more info about me somewhere there:</p>
+        <p>You can find more info about me here:</p>
         <ul className="sm-list">
           <li><a target="_blank" href="https://t.me/yana_karpliuk"><img className="sm-icon" src={telegram}></img></a></li>
           <li><a target="_blank" href="https://www.linkedin.com/in/yana-karpliuk-3820a8208/"><img className="sm-icon" src={linkedin}></img></a></li>

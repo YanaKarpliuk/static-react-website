246f8f0b8762fc13959f0a91b38a5045b69d5607
Yana Karpliuk <yanakarpliuk99@gmail.com>
2022-08-07 22:30:34 +0300

    new project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter, Route, Routes
+} from "react-router-dom";
+import "./index.css";
+import ReactFacts from './pages/FactsPage';
+import MainPage from './pages/MainPage';
+import About from './pages/About'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+function App(){
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/facts" element={<ReactFacts />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </BrowserRouter>
+)
---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import Footer from "../Footer";
+import Header from "../Header";
+import MainContent from "../MainContent";
+
+export default function MainPage() {
+  return (
+    <div className="container">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
+}
